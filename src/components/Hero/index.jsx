b15cf0682f5d3d64826915bed7eb0424501e5b19
Eirik Nordstrand Veskje <eirik.nv@hotmail.com>
2025-03-22 16:50:39 +0100

    Update hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -3,9 +3,12 @@ import HeroImage from "../../assets/images/shopping.jpg";
 export const Hero = () => {
   return (
     <>
-      <div className="flex flex-row mt-20 mx-auto max-w-6xl">
-       
-        <img src={HeroImage} alt="Hero image" />
+      <div className="flex flex-row mt-20 mx-auto max-w-5xl">
+        <img
+          src={HeroImage}
+          alt="Hero image"
+          className="shadow-lg rounded-lg"
+        />
       </div>
     </>
   );
